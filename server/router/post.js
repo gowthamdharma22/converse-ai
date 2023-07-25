22ef5f3fc70374f7ab1b56289546f3d85ef8ad30
Gowtham Dharma <105642850+gowthamdharma22@users.noreply.github.com>
2023-07-25 07:34:52 +0530

    Update post.js

--- server/router/post.js
+++ server/router/post.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const openai = new OpenAIApi(
   new Configuration({
-    apiKey: sk - aZ1kmDMiPCk0AknnSE7xT3BlbkFJpc8PCLB8GmAcQJbOFA02,
+    apiKey: "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",
   })
 );
 
